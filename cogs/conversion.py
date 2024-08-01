396a85bbed60b0a75267bfb1d8d3e7fd74c2478a
JDJG <ghostzap123@gmail.com>
2024-07-31 22:14:24 -0400

    there

--- cogs/conversion.py
+++ cogs/conversion.py
@@ -73,7 +73,7 @@ class ConversionUtil(commands.Cog):
 
             except Exception as e:
                 localized_timezone = timezone
-                traceback.print_exc(e)
+                # traceback.print_exc(e)
 
             embed = discord.Embed(
                 title="Time:",
